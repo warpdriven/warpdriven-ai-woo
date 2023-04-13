1765a68ed3bcf9b9615c8bf10ddb523850c224f5
zhangsheng <yanglee2421@gmail.com>
2023-04-13 21:18:12 +0800

    chmod

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'warp-driven/wd-woo-plugin-nlp',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'e34cb8a78c0401c484d48620a442819b6474c2e1',
+        'reference' => '603b3a3ce49786e375aec06e5c65518246d54338',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'warp-driven/php-sdk' => array(
             'pretty_version' => 'dev-dev',
             'version' => 'dev-dev',
-            'reference' => '8575fbc3382b198353a642b7b718c6e21fefa244',
+            'reference' => '0b22ebdeb5c258007d6fafa50ebe9fdba00483d8',
             'type' => 'library',
             'install_path' => __DIR__ . '/../warp-driven/php-sdk',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'warp-driven/wd-woo-plugin-nlp' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'e34cb8a78c0401c484d48620a442819b6474c2e1',
+            'reference' => '603b3a3ce49786e375aec06e5c65518246d54338',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
