04009ca7f5c4ed547bd136ec2f4d17e0b7c11524
zhangsheng <yanglee2421@gmail.com>
2023-04-21 14:17:58 +0800

    fix active_api

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'warp-driven/wd-woo-plugin-nlp',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '3574bcd802bd13de96fc0856f670c8b03e2960a1',
+        'reference' => '020b3313e9119bdcd86d772810cba7b14f39cec9',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'warp-driven/php-sdk' => array(
             'pretty_version' => 'dev-dev',
             'version' => 'dev-dev',
-            'reference' => 'cbfb1a433a5dd37a3eecdfd527d164b2d406c667',
+            'reference' => '64149d13163b90f18ae31d2be534789249077dc0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../warp-driven/php-sdk',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'warp-driven/wd-woo-plugin-nlp' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '3574bcd802bd13de96fc0856f670c8b03e2960a1',
+            'reference' => '020b3313e9119bdcd86d772810cba7b14f39cec9',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
