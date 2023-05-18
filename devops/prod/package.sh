bf2e782105555d96087f1fbdac8db504ecf918e2
zhangfujiang <18604438176@163.com>
2023-05-18 18:03:25 +0800

    rm target

--- devops/prod/package.sh
+++ devops/prod/package.sh
@@ -3,9 +3,15 @@ git pull
 composer upgrade
 cp devops/prod/WDEnv.php vendor/warp-driven/php-sdk/src/
 
+cd ../wd-woo-plugin-gpt-vue/
+git pull
 npm install
 npm run build
 
+cp -R dist ../wd-woo-plugin-nlp-php/assets/
+
+cd ../wd-woo-plugin-nlp-php
+
 mkdir -p ./target/prod
 rm ./target/prod/*.zip
-zip -r  ./target/prod/wd-gpt-copywriting.zip . -x "devops/**" "node_modules/**" "target/**" ".*"
+zip -r  ./target/prod/wd-woo-plugin-nlp-php.zip . -x "devops/**" "node_modules/**" "target/**" ".*"
--- devops/stg/package.sh
+++ devops/stg/package.sh
@@ -3,9 +3,16 @@ git pull
 composer upgrade
 cp devops/stg/WDEnv.php vendor/warp-driven/php-sdk/src/
 
+
+cd ../wd-woo-plugin-gpt-vue/
+git pull
 npm install
 npm run build
 
+cp -R dist ../wd-woo-plugin-nlp-php/assets/
+
+cd ../wd-woo-plugin-nlp-php
+
 mkdir -p ./target/stg
 rm ./target/stg/*.zip
-zip -r  ./target/stg/wd-gpt-copywriting.zip . -x "devops/**" "node_modules/**" "target/**" ".*"
+zip -r  ./target/stg/wd-woo-plugin-nlp-php.zip . -x "devops/**" "node_modules/**" "target/**" ".*"
